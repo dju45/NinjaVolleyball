2cbb7fa93f427f69a18c0e0c9a7f682a63fc044c
EmilieBRG <emibgr@gmail.com>
2018-05-18 06:25:51 +0200

    it's a final countdown without style

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,29 @@
+// Import SCSS
 require('../scss/main.scss');
+
+// CountDown
+let countDownDate = new Date("August 8, 2018 15:37:25").getTime();
+let x = setInterval(function() {
+
+    // Get todays date and time
+    let now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    let distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById('demo').innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
